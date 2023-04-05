822efc814a0273be1c1b903f4e3db0ea62fec48f
Faiza Ahmed <shamim.faiza@gmail.com>
2023-04-05 16:36:45 -0400

    2nd commit

--- src/Strings/countString.java
+++ src/Strings/countString.java
@@ -1,10 +1,9 @@
 package Strings;
 
-public class hello {
+public class countString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World 123456");
 
 	}
 
